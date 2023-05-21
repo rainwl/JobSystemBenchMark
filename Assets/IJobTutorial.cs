f84cbdf08f876d6254ad85c59bf7717ca980948a
Rain <2215338912@qq.com>
2023-05-21 10:13:15 +0800

    add: a u proj

---
+++ Assets/IJobTutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IJobTutorial : MonoBehaviour
+{
+    void Update()
+    {
+        for (int i = 0; i < 100000; i++)
+        {
+            var f = Mathf.Sqrt(Mathf.Pow(10, 100000f)) / 10000000f;
+        }
+    }
+}
